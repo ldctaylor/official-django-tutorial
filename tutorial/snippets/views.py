aaa5f69e0fdd9f91f7b9843e18f0cd3a44ac5f34
Leah <ldctaylor@gmail.com>
2023-08-12 21:28:31 +1000

    Add hyperlinked relationships

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,14 +1,21 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status, generics, permissions, mixins
+from rest_framework.reverse import reverse
+from rest_framework import generics, permissions, renderers
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
 from django.http import Http404
 from rest_framework.views import APIView
-from rest_framework.response import Response
 from django.contrib.auth.models import User
 
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
@@ -34,6 +41,14 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     List all code snippets, or create a new snippet.
     """
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 """ 
 Before refactor with generic class based views: 
